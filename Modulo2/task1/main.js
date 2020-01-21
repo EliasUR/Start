e13d6e63aa3f92e6af8ec986df903564f17f9e5d
EliasUR <59623052+EliasUR@users.noreply.github.com>
2020-01-21 17:45:18 -0300

    Add files via upload

---
+++ Modulo2/task1/main.js
@@ -0,0 +1,28 @@
+var algo="Starting javascript..."
+console.log (algo)
+
+console.log ("Writing expressions with variables")
+
+console.log ("Exercise 1: myName")
+var myName="Elías"
+console.log (myName)
+
+console.log ("Exercise 2: age")
+var age=17
+console.log (age)
+
+console.log ("Exercise 3: ageDiff")
+var ignasiAge=32
+var ageDiff= age - ignasiAge
+console.log (ageDiff)
+
+console.log ("Writing code with conditionals")
+
+console.log ("Exercise 4")
+if (age>21){console.log("You are older than 21")}
+	else{console.log("You are not older than 21")}
+
+console.log ("Exercise 5")
+if (age>ignasiAge) {console.log ("Ignasi is younger than you")}
+	else if (age<ignasiAge) {console.log ("Ignasi is older than you")}
+		else {console.log ("You have the same age as Ignasi")}
