b417a28a54f99508cdaca39459fa704ed4132c88
EliasUR <eliasrodriguez2002@hotmail.com>
2020-02-26 23:03:20 -0300

    hola

---
+++ Modulo3/task2/scripts/schedule.js
@@ -0,0 +1,109 @@
+var schedule = {
+	september:{
+		"01":{
+			"9:30 a.m.":{
+				zona:"AJ Katzenmaier",
+				team1:"U1",
+				team2:"U4",
+			}, 
+			"1:00 p.m.":{
+				zona:"Greenbay",
+				team1:"U3",
+				team2:"U2",
+			},
+		},
+		"08":{
+			"9:30 a.m.":{
+				location:"Howard A Yeager",
+				team1:"U5",
+				team2:"U6",
+			}, 
+			"1:00 p.m.":{
+				location:"Marjorie P Hart",
+				team1:"U6",
+				team2:"U1",
+			},
+		},
+		"15":{
+			"9:30 a.m.":{
+				location:"North",
+				team1:"U2",
+				team2:"U4",
+			}, 
+			"1:00 p.m.":{
+				location:"AJ Katzenmaier",
+				team1:"U3",
+				team2:"U5",
+			},
+		},
+		"22":{
+			"9:30 a.m.":{
+				location:"South",
+				team1:"U1",
+				team2:"U3",
+			}, 
+			"1:00 p.m.":{
+				location:"Howard A Yeager",
+				team1:"U2",
+				team2:"U6",
+			},
+		},
+		"29":{
+			"9:30 a.m.":{
+				location:"Greenbay",
+				team1:"U4",
+				team2:"U5",
+			},
+		},
+	},
+	october:{
+		"06":{
+			"9:30 a.m.":{
+				location:"Marjorie P Hart",
+				team1:"U2",
+				team2:"U5",
+			}, 
+			"1:00 p.m.":{
+				location:"South",
+				team1:"U1",
+				team2:"U6",
+			},
+		},
+		"08":{
+			"9:30 a.m.":{
+				location:"Howard A Yeager",
+				team1:"U3",
+				team2:"U4",
+			}, 
+			"1:00 p.m.":{
+				location:"Greenbay",
+				team1:"U5",
+				team2:"U1",
+			},
+		},
+		"20":{
+			"9:30 a.m.":{
+				location:"North",
+				team1:"U6",
+				team2:"U3",
+			}, 
+			"1:00 p.m.":{
+				location:"Marjorie P Hart",
+				team1:"U2",
+				team2:"U4",
+			},
+		},
+		"27":{
+			"9:30 a.m.":{
+				location:"AJ Katzenmaier",
+				team1:"U3",
+				team2:"U1",
+			}, 
+			"1:00 p.m.":{
+				location:"Howard A Yeager",
+				team1:"U5",
+				team2:"U6",
+			},
+		},
+	},
+}
