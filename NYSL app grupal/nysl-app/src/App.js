60a64a1433c6ec50174e1bbd6dd8e628540be2b2
EliasUR <eliasrodriguez2002@hotmail.com>
2020-03-08 22:58:39 -0300

    Esto no sirve pero we

---
+++ NYSL app grupal/nysl-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './components/Header';
+import Tablero from './components/Tablero';
+
+
+const App = () => {
+
+    
+    return ( 
+        <div className="app">
+            <Header/>
+            <Tablero/>
+        </div>
+     );
+}
+ 
+export default App;
